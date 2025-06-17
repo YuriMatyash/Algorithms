9cb1b4c2ab254dcce98163fd49e0d1f4330f37d6
YuriMatyash <yurimatyash.bsc@gmail.com>
2025-06-17 18:32:12 +0300

    implemented undirected/directed graph classes

--- structures/graphDirected.py
+++ structures/graphDirected.py
@@ -3,7 +3,19 @@ from .node import Node
 
 class GraphDirected(Graph):
     def addEdge(self, edge: tuple[Node, Node]) -> None:
-        return
+        source, goal = edge
+
+        if source not in self.adj_list.keys():              # Source node is not in the graph
+            return
+        if goal not in self.adj_list.keys():                # Goal node is not in the graph, prevent linking non-existant node
+            return
+        if goal not in self.adj_list[source]:               # add goal node to source node's neighbor's list
+            self.adj_list[source].append(goal)
 
     def removeEdge(self, edge: tuple[Node, Node]) -> None:
-        return
+        source, goal = edge
+
+        if source not in self.adj_list.keys():              # Source node is not in the graph
+            return
+        if goal in self.adj_list[source]:                   # remove goal node from source node's neighbor's list
+            self.adj_list[source].remove(goal)
--- structures/graphUndirected.py
+++ structures/graphUndirected.py
@@ -1,9 +1,29 @@
 from .graph import Graph
 from .node import Node
 
-class GraphUnirected(Graph):
+class GraphUndirected(Graph):
     def addEdge(self, edge: tuple[Node, Node]) -> None:
-        return
+        A,B = edge
+
+        if A not in self.adj_list.keys():               # A node is not in the graph
+            return
+        if B not in self.adj_list.keys():               # B node is not in the graph
+            return
+        
+        if B not in self.adj_list[A]:                   # add B node to A node's neighbor's list
+            self.adj_list[A].append(B)
+        if A not in self.adj_list[B]:                   # add A node to B node's neighbor's list
+            self.adj_list[B].append(A)
 
     def removeEdge(self, edge: tuple[Node, Node]) -> None:
-        return
+        A,B = edge
+
+        if A not in self.adj_list.keys():           # A node is not in the graph
+            return
+        if B in self.adj_list[A]:                   # remove B node from A node's neighbor's list
+            self.adj_list[A].remove(B)
+
+        if B not in self.adj_list.keys():           # B node is not in the graph
+            return
+        if A in self.adj_list[B]:                   # remove A node from B node's neighbor's list
+            self.adj_list[B].remove(A)
